9c85c939b749c374b98ca67c91af3d519f10a2d2
hss <HssInspiration@aliyun.com>
2020-05-27 23:14:43 +0800

    创建  springboot  demo

---
+++ src/test/java/com/inspiration/springboot/backstage/BackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspiration.springboot.backstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
